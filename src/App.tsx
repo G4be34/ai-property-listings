de90220a2ed9869a17957056238e06749721846b
Gabe Jimenez <jimenezgabriel12@gmail.com>
2025-01-30 04:47:08 -0700

    Add exit and entrance animations and change clickable area for each listing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useState } from 'react';
 import { CiDollar, CiPaperplane } from 'react-icons/ci';
 import { MdLightbulbOutline } from 'react-icons/md';
@@ -325,14 +326,14 @@ function App() {
       const preferences = formData.get('preferences') as string;
 
       // todo: integrate the backend directly into this project so the temporary vercel endpoint isn't needed
-      const res = await axios.post('https://vite-react-theta-two-40.vercel.app/search', { text: preferences, page: 1 });
-      const listingRecords = res.data.matches as ListingRecord[];
-      const listings = mapListings(listingRecords);
+      // const res = await axios.post('https://vite-react-theta-two-40.vercel.app/search', { text: preferences, page: 1 });
+      // const listingRecords = res.data.matches as ListingRecord[];
+      // const listings = mapListings(listingRecords);
 
       console.log("Listings:", listings);
 
-      setListings(listings);
-      setVisibleListings(listings.slice(0, 6));
+      setListings(exampleListings);
+      setVisibleListings(exampleListings.slice(0, 6));
 
       setIsDataLoaded(true);
     } catch (error) {
@@ -372,44 +373,44 @@ function App() {
         <h1 className='header-title' onClick={() => window.location.reload()}>Finding Places</h1>
         <ThemeToggle />
       </header>
-      {listings.length === 0 && !isDataLoaded
-        ? <>
-            <h2 className='title'>AI-Powered home search and property inspection</h2>
-            <div className='description-container'>
-              <p className='description'>I'll help you find your ideal apartment. Share your preferences for location, budget, and features, and I'll do the rest</p>
-              <p className='description'>Or I can help you in other ways shown below!</p>
-            </div>
-            <div className='command-grid-container'>
-              {presetCommands.map((section, index) => (
-                <div key={index} className='command-container' onClick={() => commandClick(section.command)}>
-                  <div className='command-icon'>{section.icon}</div>
-                  <p className='command'>{section.command}</p>
-                </div>
-              ))}
-            </div>
-          </>
-        : <div className="hide-elements" />}
-      <form onSubmit={submitPreference} className='preferences-form'>
-        <input className='preferences-input' type="text" name="preferences" placeholder='Tell me your preferences' />
-        <button type='submit' className='submit-button' disabled={isLoading || loadingCommand}>
-          {isLoading ? <div className='loader'></div> : <CiPaperplane size={25} style={{ strokeWidth: 0.5}} />}
-        </button>
-      </form>
+      <AnimatePresence>
+        {listings.length === 0
+          && <motion.div key="options" exit={{ opacity: 0, y: -50 }} transition={{ duration: 0.5}}>
+              <h2 className='title'>AI-Powered home search and property inspection</h2>
+              <div className='description-container'>
+                <p className='description'>I'll help you find your ideal apartment. Share your preferences for location, budget, and features, and I'll do the rest</p>
+                <p className='description'>Or I can help you in other ways shown below!</p>
+              </div>
+              <div className='command-grid-container'>
+                {presetCommands.map((section, index) => (
+                  <div key={index} className='command-container' onClick={() => commandClick(section.command)}>
+                    <div className='command-icon'>{section.icon}</div>
+                    <p className='command'>{section.command}</p>
+                  </div>
+                ))}
+              </div>
+            </motion.div>}
+        <motion.form layout onSubmit={submitPreference} className='preferences-form'>
+          <input className='preferences-input' type="text" name="preferences" placeholder='Tell me your preferences' />
+          <button type='submit' className='submit-button' disabled={isLoading || loadingCommand}>
+            {isLoading ? <div className='loader'></div> : <CiPaperplane size={25} style={{ strokeWidth: 0.5}} />}
+          </button>
+        </motion.form>
+      </AnimatePresence>
       <div className='powered-by-container'>
         <small className='powered-by'>Powered by GPT-4</small>
       </div>
       {listings.length > 0
-        ? <>
+        && <motion.div initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1.5, delay: 0.5 }}>
             <div className='listing-grid-container'>
               {visibleListings.map((listing, index) => (
-                <ListingCard key={index} listing={listing} />
+                <ListingCard key={index} listing={listing} setShowModal={setShowModal} />
               ))}
             </div>
             {visibleListings.length < listings.length
               ? <button className='load-more-button' onClick={loadMoreListings}>Load more</button>
               : null}
-          </>
-        : null}
+          </motion.div>}
     </>
   )
 }
--- src/components/ListingCard/ListingCard.tsx
+++ src/components/ListingCard/ListingCard.tsx
@@ -1,18 +1,19 @@
+import { motion } from "framer-motion";
 import { RiPokerDiamondsLine } from "react-icons/ri";
 import { Listing } from "../../utils";
 import "./ListingCard.css";
 
-export function ListingCard({ listing }: { listing: Listing }) {
+export function ListingCard({ listing, setShowModal }: { listing: Listing, setShowModal: React.Dispatch<React.SetStateAction<boolean>> }) {
   const openListing = () => {
     window.open(listing.link, "_blank");
   };
 
   return (
-    <div className="listing-card" onClick={openListing}>
+    <motion.div initial={{ opacity: 0, y: 25 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1.5, delay: 0.5 }} className="listing-card">
       <h2 className="listing-card-title">{listing.title}</h2>
       <p className="listing-card-price">{listing.price}</p>
       <small className="listing-card-address">{listing.address}</small>
-      <div className="listing-card-main-img-container">
+      <div className="listing-card-main-img-container"  onClick={openListing}>
         <img src={listing.mainImg} alt={listing.title} />
       </div>
       <div className="listing-card-pros-container">
@@ -25,8 +26,8 @@ export function ListingCard({ listing }: { listing: Listing }) {
             <li key={index}><small>{pro}</small></li>
           ))}
         </ul>
-        <button className="listing-card-button" type="button" onClick={openListing}>I want this checked out</button>
+        <button className="listing-card-button" type="button" onClick={() => setShowModal(true)}>I want this checked out</button>
       </div>
-    </div>
+    </motion.div>
   )
 };
