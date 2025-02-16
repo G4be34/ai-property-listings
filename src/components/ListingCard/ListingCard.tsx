db8d748ad2ae2a4ebb4066955deafd468021ffa9
Gabe Jimenez <jimenezgabriel12@gmail.com>
2025-02-16 15:35:06 -0700

    Move style updates to main pc

--- src/components/ListingCard/ListingCard.tsx
+++ src/components/ListingCard/ListingCard.tsx
@@ -10,25 +10,12 @@ export function ListingCard({ listing, setShowModal }: { listing: Listing, setSh
 
   return (
     <motion.div initial={{ opacity: 0, y: 25 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1.5, delay: 0.5 }} className="listing-card">
-      <h2 className="listing-card-title">{listing.title}</h2>
-      <p className="listing-card-price">{listing.price}</p>
-      <small className="listing-card-address">{listing.address}</small>
       <div className="listing-card-main-img-container"  onClick={openListing}>
         <img src={listing.mainImg} alt={listing.title} />
       </div>
       <div className="listing-card-pros-container">
-        {listing.pros.length > 0
-          && <>
-              <div className="listing-card-title-container">
-                <RiPokerDiamondsLine style={{ color: "white" }} />
-                <span>Your requirements</span>
-              </div>
-              <ul className="listing-card-pros-list">
-                {listing.pros.map((pro, index) => (
-                  <li key={index}><small>{pro}</small></li>
-                ))}
-              </ul>
-             </>}
+        <h2 className="listing-card-price">{listing.price}</h2>
+        <small className="listing-card-address">{listing.title} | {listing.address}</small>        
         <button className="listing-card-button" type="button" onClick={() => setShowModal(true)}>I want this checked out</button>
       </div>
     </motion.div>
